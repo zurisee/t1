49a8b26d1c41b431615f60b3693587b0ad716843
sto <sto@usility.ch>
2014-02-27 20:00:09 +0100

    inital

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html lang="de">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<title>Test 1</title>
+</head>
+<body>
+
+<h1>Test 1</h1>
+
+
+</body>
+</html>
